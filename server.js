d394fc7ef3c621ea86c1f4cd0c8322de191b8b25
AbcFarzaan <farzaanbukhari1999@gmail.com>
2022-01-06 01:52:43 +0500

    ty

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const db = knex ({
     connectionString: process.env.DATABASE_URL,
     ssl: {
     rejectUnauthorized: false,
-    },
-    },
+    }
+  }
 });
 
 // db.select('*').from('users').then(data => {
