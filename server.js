36bd53ee55b530b7c19165dd05f58401cc8c25d4
AbcFarzaan <farzaanbukhari1999@gmail.com>
2022-01-05 23:04:10 +0500

    Fixed port

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(cors());
 app.use(express.json());
 
 
-app.get('/', (req, res)=> { res.send(db.users) })
+app.get('/', (req, res)=> { res.send("The app is working!") })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
@@ -35,6 +35,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
-})
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`);
+  });
